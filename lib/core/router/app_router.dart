49eaef73c57a889460b9e6b4705c3e01767cc724
Moaz Osama <wwwmoazosama525@gmail.com>
2025-10-23 20:05:04 +0300

    fix : app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,3 @@
-import 'package:elevate_super_fitness/core/custom_widget/custom_auth_bg.dart';
-import 'package:elevate_super_fitness/core/custom_widget/custom_glass_shape_widget.dart';
 import 'package:elevate_super_fitness/core/router/route_names.dart';
 import 'package:elevate_super_fitness/presentation/on_boarding/screen/on_boarding_screen.dart';
 import 'package:elevate_super_fitness/presentation/splash/screen/splash_screen.dart';
@@ -15,7 +13,7 @@ abstract class AppRouter {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
 
       case RouteNames.onBoarding:
-        return MaterialPageRoute(builder: (_) => const CustomTextField());
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
 
       case RouteNames.login:
         return MaterialPageRoute(builder: (_) => const SizedBox());
@@ -23,22 +21,5 @@ abstract class AppRouter {
       default:
         return MaterialPageRoute(builder: (_) => const SizedBox());
     }
-  }
-}
-
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomAuthBg(
-        child: Column(
-          children: [
-            CustomGlassShapeWidget(child: Column(children: [TextFormField()])),
-          ],
-        ),
-      ),
-    );
   }
 }
