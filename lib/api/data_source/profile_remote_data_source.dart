a3e5ab8d2b164c0c0858178d4b286c158bcc7acc
Moaz Osama <wwwmoazosama525@gmail.com>
2025-10-29 23:27:00 +0300

    feat: add meals client

---
+++ lib/api/data_source/profile_remote_data_source.dart
@@ -0,0 +1,4 @@
+ import 'package:elevate_super_fitness/data/data_source/profile_remote_data_source.dart';
+import 'package:injectable/injectable.dart';
+@Injectable(as: ProfileRemoteDataSource)
+class ProfileRemoteDataSourceImpl  implements ProfileRemoteDataSource{}
