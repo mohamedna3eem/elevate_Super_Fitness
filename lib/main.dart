442da944ee19509b76b41447eee59b95b21cc175
Moaz Osama <wwwmoazosama525@gmail.com>
2025-11-01 00:43:26 +0200

    fix:change navigation

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: AppTheme.lightTheme,
           onGenerateRoute: AppRouter.onGenerateRoute,
-          initialRoute: RouteNames.exercise,
+          initialRoute: RouteNames.splash,
           localizationsDelegates: [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
