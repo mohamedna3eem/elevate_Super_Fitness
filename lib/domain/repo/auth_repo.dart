9b6b3da3241d5ed9ef4f78def6a2b8069db87221
nmo86 <nmo865047@gmail.com>
2025-11-01 07:36:02 +0200

    implement Workouts Page with muscles tabs and exercises grid

--- lib/domain/repo/auth_repo.dart
+++ lib/domain/repo/auth_repo.dart
@@ -1,7 +1,6 @@
 import 'package:elevate_super_fitness/core/api_result/api_result.dart';
 import 'package:elevate_super_fitness/domain/entites/login_entity.dart';
 import 'package:elevate_super_fitness/domain/entites/requests/login_request_entity.dart';
-import 'package:injectable/injectable.dart';
 
 abstract interface class AuthRepo {
   Future<ApiResult<LoginResponseEntity>> login({
