b830d88b558057b3064718470b4737b222ae6740
nmo86 <nmo865047@gmail.com>
2025-11-07 08:50:36 +0200

    implement Profile Page with profile data and logout feature

--- lib/presentation/profile_page/view/widgets/CustomprofileRow.dart
+++ lib/presentation/profile_page/view/widgets/CustomprofileRow.dart
@@ -1,13 +1,13 @@
 import 'package:elevate_super_fitness/core/constants/app_colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomProfileRow extends StatelessWidget {
-  String icon;
-  String tittle;
-  IconData icon2;
-   CustomProfileRow({
+ final String icon;
+ final String tittle;
+ final IconData icon2;
+   const CustomProfileRow(
+       {super.key,
      required this.icon,
      required this.tittle,
      required this.icon2,
